a8a2d32408a78a0dfe2675aef612aaec0a7ffa9a
JaydipMer <81605768+JaydipBacancy@users.noreply.github.com>
2025-05-05 12:30:51 +0530

    Added routing

--- Client/src/app/nav/nav.component.ts
+++ Client/src/app/nav/nav.component.ts
@@ -1,30 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-   accountService = inject(AccountService)
+   accountService = inject(AccountService);
+   private route = inject(Router);
+   private toastr = inject(ToastrService)
   loginModel:any={}
   
 
   Login(){
     this.accountService.login(this.loginModel).subscribe({
       next: response =>{
-        console.log(response);
+        this.route.navigateByUrl('/members');
+        this.toastr.success('Logged in successfully');
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     })
 
   }
 
   Logout(){
     this.accountService.logout();
+    this.route.navigateByUrl('/');
   }
 
 }
